66f8a46bc988f8f9ce519809f099e9e982b88be1
EDUARDO VALENCIA <8025918@gmail.com>
2021-08-12 13:03:39 -0700

    Add merge sort header

---
+++ csc382-sort-2/mergeSort.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Node.h"
+#include "LinkedList.h"
+
+template <typename Data>
+void moveNodeIntoSortedList(Node<Data> & node);
+
+template <typename Data>
+Node<Data>* getHeadOfListWithRemainingElements(LinkedList<Data>& leftList, LinkedList<Data>& rightList);
+
+template <typename Data>
+void moveRemainingElements(LinkedList<Data>& sortedItems, LinkedList<Data>& leftList, LinkedList<Data>& rightList);
+
+template <typename Data>
+LinkedList<Data> mergeLists(LinkedList<Data>& leftList, LinkedList<Data>& rightList);
+
+template <typename Data>
+Node<Data>* getMiddleNode(LinkedList<Data>& list);
+
+template <typename Data>
+LinkedList<Data> mergeSort(LinkedList<Data>& list);
