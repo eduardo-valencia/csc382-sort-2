2b82c85571378b663af3050cc7738ca317c71200
Eduardo Valencia <8025918@gmail.com>
2021-08-14 13:49:52 -0700

    Remove function to get head of list with remaining elements and update types

--- csc382-sort-2/mergeSort.h
+++ csc382-sort-2/mergeSort.h
@@ -6,19 +6,16 @@
 #include "LinkedList.h"
 
 template <typename Data>
-void moveNodeIntoSortedList(LinkedList<Data>& list, LinkedList<Data>& sortedItems, Node<Data> & node);
+void moveNodeIntoSortedList(LinkedList<Data> &list, LinkedList<Data> &sortedItems, Node<Data> *node);
 
 template <typename Data>
-Node<Data>* getHeadOfListWithRemainingElements(LinkedList<Data>& leftList, LinkedList<Data>& rightList);
+void moveRemainingElements(LinkedList<Data> &sortedItems, LinkedList<Data> &leftList, LinkedList<Data> &rightList);
 
 template <typename Data>
-void moveRemainingElements(LinkedList<Data>& sortedItems, LinkedList<Data>& leftList, LinkedList<Data>& rightList);
+LinkedList<Data> mergeLists(LinkedList<Data> &leftList, LinkedList<Data> &rightList);
 
 template <typename Data>
-LinkedList<Data> mergeLists(LinkedList<Data>& leftList, LinkedList<Data>& rightList);
+Node<Data> *getMiddleNode(LinkedList<Data> &list);
 
 template <typename Data>
-Node<Data>* getMiddleNode(LinkedList<Data>& list);
-
-template <typename Data>
-LinkedList<Data> mergeSort(LinkedList<Data>& list);
+LinkedList<Data> mergeSort(LinkedList<Data> &list);
