74a1785e3800902fe04feaf2b1ffa974453a00fb
Eduardo Valencia <8025918@gmail.com>
2021-08-14 13:48:46 -0700

    Add doxyfile and comments

--- csc382-sort-2/DynamicSortTest.h
+++ csc382-sort-2/DynamicSortTest.h
@@ -8,10 +8,16 @@
 
 using namespace std;
 
+///
+/// The class that will handle testing the sort function manually.
+///
 class DynamicSortTest : public DynamicTest
 {
 public:
 	DynamicSortTest();
 
+	///
+	/// The function that will test merge sort.
+	///
 	void testOperation();
 };
